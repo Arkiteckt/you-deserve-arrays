9ace0fe73ff99614486100b4070ac0e90a4f8dfd
orlando.tyrell <orlandotyrell@gmail.com>
2022-06-15 10:43:44 -0400

    getLastItemFrom.js

---
+++ getLastItemFrom.js
@@ -0,0 +1,19 @@
+const prompt =  require('prompt-sync')({ sigint: true});
+
+let userArray = (prompt("Can you give me an Array Barray?: "));
+
+var randomStuff = ["go cart", "helicopter", "motorcycle"]
+
+var x = 0
+console.log(x > [randomStuff.length - 1]);
+//{
+    //console.log (randomStuff[x]);
+   // x--
+//}
+
+
+//Write a program, getLastItemFrom.js that prompts the user for an array, then logs
+// out the last item from that array
+
+
+//const lastItem = colors[colors.length -1]
