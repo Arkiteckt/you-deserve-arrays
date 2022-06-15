0a24de1805ef9f3e480d34bd0de988441dbd686f
orlando.tyrell <orlandotyrell@gmail.com>
2022-06-15 10:42:02 -0400

    getFirstItemFrom.js

---
+++ getFirstItemFrom.js
@@ -0,0 +1,21 @@
+
+const prompt =  require('prompt-sync')({ sigint: true});
+
+let userArray = (prompt("Can you give me an Array Barray?: "));
+
+var randomStuff = ["go cart", "helicopter", "motorcycle"]
+
+var x = 0
+console.log(x < randomStuff.length)
+{
+    console.log (randomStuff[x]);
+    
+}
+
+
+
+
+
+//Write a program, getFirstItemFrom.js that prompts the user for an array 
+//then logs out the first item in that array
+
